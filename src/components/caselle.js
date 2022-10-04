bd321a7ab7c89f616eb002f0874ca31f596e4e87
Matteo_G <honropgiochi@gmail.com>
2022-10-04 10:25:20 +0200

    manca condizione di parità...codice OK

--- src/components/caselle.js
+++ src/components/caselle.js
@@ -1,8 +1,18 @@
 import React from 'react'
-export default function Casella({ onClick, value }) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faO } from '@fortawesome/free-solid-svg-icons';
+export default function Casella({ onClick, stato}) {
+    if(stato===0){
+      stato=<span> </span>;
+    }else if(stato===1){
+      stato= <FontAwesomeIcon icon={faXmark}/>
+    }else{
+      stato= <FontAwesomeIcon icon={faO}/>
+    }
     return (
       <button className="square" onClick={onClick}>
-        {value}
+      {stato}
       </button>
     );
-  }
+}
--- src/components/tabella.js
+++ src/components/tabella.js
@@ -5,48 +5,52 @@ import Calcolovincitore from "../vincitore.js"
 import Casella from './caselle.js'
 
 export default function Board () {
-  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  const [squares, setSquares] = React.useState(Array(9).fill(0))
   const [isX, setIsX] = React.useState(true);
 
   const Click = (i) => {
     if (Calcolovincitore(squares) || squares[i]) {
       return
     }
-    
-    squares[i] = isX ? <FontAwesomeIcon icon={faXmark}/> : <FontAwesomeIcon icon={faO}/>
+    squares[i] = isX ? 1 : 2 ;
     setSquares(squares)
     setIsX(!isX)
   }
 
+  function Turnodi(){
   const vincitore = Calcolovincitore(squares)
-  let status=0;
-  
-  if (vincitore) {
-    status = (<span> Vincitore: ${vincitore} </span>);
+  let stato=0;
+  if (vincitore===1) {
+    stato = <span> Vincitore: <FontAwesomeIcon icon={faXmark}/></span>
+  } else if (vincitore===2) {
+    stato = <span> Vincitore: <FontAwesomeIcon icon={faO}/></span>
   } else {
-    status = (<span>Tocca a: <FontAwesomeIcon icon={isX ? faXmark : faO}/> </span>)
+    stato = (<span>Tocca a: <FontAwesomeIcon icon={isX ? faXmark : faO}/> </span>)
   }
+  return (
+    stato
+  )
+}
   
   const Restart = () => {
     setIsX(true)
-    setSquares(Array(9).fill(null))
+    setSquares(Array(9).fill(0))
   }
 
   
   let lista = []
   for(let row=0; row<3; row++){
     let rowEL= (<div key={row} className='row-board'>
-      {[0,1,2].map((n , col) => <Casella key={row*3+col} onClick={(squares[row*3+col]) => Click(row*3+col)} /> )}
+      {[0,1,2].map((col) => <Casella key={row*3+col} stato={squares[row*3+col]} onClick={() => Click(row*3+col)} /> )}
     </div>)
     lista.push(rowEL)
   }
+
   return (
-    <>
     <div className="board">
        {lista}
-    <div className="status">{status}</div>
+    <div className="stato">{Turnodi()}</div>
     <button className="restart" onClick={Restart}>---Rigioca---</button>
     </div>
-    </>
   )
 }
